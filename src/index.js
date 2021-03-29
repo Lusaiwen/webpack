adf93237737fc9cc49e37b8851b61c37b1b60d50
Lusaiwen <1811977648@qq.com>
2021-03-29 11:04:18 +0800

    webpack拆分css

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var str = require('./assets/banner.css')
+
+console.log(str.default.toString());
+
+console.log(str);
+
+// var result = str.default.toString();
+
+
+// const style = document.createElement('style');
+// style.innerHTML = result;
+
+// document.head.appendChild(style);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,21 @@
-const baseConfig = require('./webpack.base');
-const devConfig = require('./webpack.dev');
-const prodConfig = require('./webpack.prod');
+const path = require('path');
 
-
-module.exports = function (env) {
-  console.log(env);
-  if(env && env.prod) {
-    //生产环境
-    var config = {
-      ...baseConfig,
-      ...prodConfig
-    }
-    config.plugins = [...baseConfig.plugins, ...prodConfig.plugins]
-    return config;
-  }
-  else {
-    //开发环境
-    return {
-      ...baseConfig,
-      ...devConfig
-    }
+module.exports = {
+  mode: 'development',
+//   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/, use: ['style-loader' , 'css-loader']
+      },
+      {
+        test: /\.png$/, use: 'file-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+    port: 8000
   }
 }
